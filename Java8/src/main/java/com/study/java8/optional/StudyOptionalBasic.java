3b9fcb0c6657dbf66ada1288ed909c13776279e0
Largem <largemtan@gmail.com>
2020-11-26 00:34:18 -0500

    study java 8 optional basic

--- Java8/src/main/java/com/study/java8/optional/StudyOptionalBasic.java
+++ Java8/src/main/java/com/study/java8/optional/StudyOptionalBasic.java
@@ -1,4 +1,46 @@
 package com.study.java8.optional;
 
+import java.util.Optional;
+
 public class StudyOptionalBasic {
+
+    public static void studyOptionalBasic() {
+        System.out.println(StudyOptionalBasic.class.getName());
+        final Optional<String> optionalWithValue = optionalIsForReturn(true);
+
+        final Optional<String> emptyOptional = optionalIsForReturn(false);
+
+        // convert Optional value
+        int stringLength = optionalWithValue.map(String::length).orElse(0);
+        // provide default value for an empty optional
+        String defaultValue = emptyOptional.orElse("DefaultValue");
+        // provide default value from heavy operation
+        String defaultValueFromOp = emptyOptional.orElseGet(StudyOptionalBasic::calculateValue);
+
+        //consuming optional
+        optionalWithValue.ifPresent(System.out::println);
+
+        //get the value from non-empty optional, not recommended (using functional style above is better.
+        if (optionalWithValue.isPresent()) {
+            System.out.println(optionalWithValue.get());
+        }
+
+        try {
+            emptyOptional.orElseThrow(() -> new RuntimeException("empty optional can not be handled"));
+        }catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String calculateValue() {
+        return "value from heavy calculation";
+    }
+
+    private static Optional<String> optionalIsForReturn(boolean cond) {
+        return cond ? Optional.of("Value") : Optional.empty();
+    }
+
+    private static Optional<String> optionalIsForReturn_fromNullable(String input) {
+        return Optional.ofNullable(input);      //create an optional from a nullable value
+    }
 }
