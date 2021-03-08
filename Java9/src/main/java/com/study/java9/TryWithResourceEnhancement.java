3ebbca9c0cfd35932a40e5abca78093e6678bafb
Largem <largemtan@gmail.com>
2021-03-07 20:38:04 -0500

    Java9: Try With Resource Enhancement

---
+++ Java9/src/main/java/com/study/java9/TryWithResourceEnhancement.java
@@ -0,0 +1,29 @@
+package com.study.java9;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TryWithResourceEnhancement {
+    public static void main(String[] args) throws IOException {
+        final InputStream inputStream1 = getInputStream("something");
+        final InputStream inputStream2 = getInputStream("something else");
+
+        try(inputStream1; inputStream2) {
+            System.out.println(new String(inputStream1.readAllBytes()));
+            System.out.println(new String(inputStream2.readAllBytes()));
+        }
+
+        System.out.println("Study TryWithResourceEnhancement Completed");
+    }
+
+    private static InputStream getInputStream(String contents) {
+        return new ByteArrayInputStream(contents.getBytes()) {
+            @Override
+            public void close() throws IOException {
+                System.out.println("closing the stream with contents \"" + contents + "\"");
+                super.close();
+            }
+        };
+    }
+}
